62af1b28321790d34464e8596ab8b99b2477d2a2
Thanh Liem <liembsbs123@gmail.com>
2021-09-08 12:11:45 +0700

    Add test folder and parse data json and urlencoded

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express from "express";
 import lusca from "lusca";
 import compression from "compression";
-import { json, urlencoded } from "body-parser";
 
 // Create Express Server
 const app = express();
 
 app.set("port", process.env.PORT || 5000);
 // parse application/x-www-form-urlencoded
-app.use(urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 // parse application/json
-app.use(json());
+app.use(express.json());
 
 app.use(lusca.xframe("SAMEORIGIN"));
 app.use(lusca.xssProtection(true));
---
+++ test/app.test.ts
@@ -0,0 +1,14 @@
+import request from "supertest";
+import app from "../src/app";
+
+describe("GET /", () => {
+  it("should return 200", (done) => {
+    request(app).get("/").expect(200, done);
+  });
+});
+
+describe("GET error path", () => {
+  it("should return 200", (done) => {
+    request(app).get("/12312312asdad").expect(404, done);
+  });
+});
